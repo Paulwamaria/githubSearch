08e0a3ba603c15266d47c253a30f741e47149988
Paulwamaria <paulwamaria@gmail.com>
2019-08-24 05:11:59 +0300

    Add a binding property to the searchTerm

--- src/app/search-form/search-form.component.ts
+++ src/app/search-form/search-form.component.ts
@@ -6,7 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./search-form.component.css']
 })
 export class SearchFormComponent implements OnInit {
-
+  searchTerm: string;
   constructor() { }
 
   ngOnInit() {
