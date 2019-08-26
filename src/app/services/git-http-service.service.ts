24dd89e5dcf84ef07e40e083d31fcaac8fb9897f
Paulwamaria <paulwamaria@gmail.com>
2019-08-26 10:47:39 +0300

    Add more styles

--- src/app/services/git-http-service.service.ts
+++ src/app/services/git-http-service.service.ts
@@ -2,14 +2,13 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { environment } from '../../environments/environment';
 import { Repos } from '../repos';
-import { promise } from 'protractor';
+// import { promise } from 'protractor';
 
 @Injectable({
   providedIn: 'root'
 })
 export class GitHttpServiceService {
   repos: Repos[] = [];
-  gitAPI='78bf3cd53966187cbfc945e904e336d101a7e1c6';
   constructor(private http: HttpClient) {
 
    }
@@ -23,7 +22,7 @@ export class GitHttpServiceService {
     // https://api.github.com/users/Paulwamaria/repos?api_key=78bf3cd53966187cbfc945e904e336d101a7e1c6
     let gitSearchEndPoint = 'https://api.github.com/users/';
     let promise =  new Promise((resolve, reject) => {
-      this.http.get<Results>(gitSearchEndPoint + searchTerm + '/repos?api_key=' + this.gitAPI).toPromise().then(
+      this.http.get<Results>(gitSearchEndPoint + searchTerm + '/repos?api_key=' + environment.GITHUBAPIKEY).toPromise().then(
         (result: any) => {
 
           this.repos = [];
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,4 @@
 export const environment = {
-  production: true
+  production: true,
+  GITHUBAPIKEY: '78bf3cd53966187cbfc945e904e336d101a7e1c6'
 };
